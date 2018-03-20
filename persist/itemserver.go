29e4b0efc964d7952b5fdcd3df4d0c8d2a3a1804
郝大全 <haodaquan@shoplinq.cn>
2018-03-20 22:54:45 +0800

    elastic存储

--- persist/itemserver.go
+++ persist/itemserver.go
@@ -7,7 +7,12 @@
 *************************************************************/
 package persist
 
-import "log"
+import (
+	"log"
+
+	"golang.org/x/net/context"
+	"gopkg.in/olivere/elastic.v5"
+)
 
 func ItemServer() chan interface{} {
 	out := make(chan interface{})
@@ -15,10 +20,35 @@ func ItemServer() chan interface{} {
 		itemCount := 0
 		for {
 			item := <-out
-			log.Printf("item saver: got item #%d %v", itemCount, item)
+			//log.Printf("item saver: got item #%d %v", itemCount, item)
 			itemCount++
+			id, err := save(item)
+			if err != nil {
+				log.Printf("err #%d %v", itemCount, err)
+			}
+
+			log.Printf("ok #%s", id)
 		}
 
 	}()
 	return out
+}
+
+func save(item interface{}) (string, error) {
+	client, err := elastic.NewClient(elastic.SetSniff(false))
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := client.Index().
+		Index("data_profile").
+		Type("zhenai").
+		BodyJson(item).
+		Do(context.Background())
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Id, nil
+
 }
